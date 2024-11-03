c092b17843bba6537c0afee943991c457f269366
Ostap4ello <okpeleh@gamil.com>
2024-11-03 22:59:57 +0100

    nvim: disable format-on-save, binds

--- .config/nvim/lua/plugins/formatter-conform.lua
+++ .config/nvim/lua/plugins/formatter-conform.lua
@@ -3,30 +3,29 @@ return { -- Autoformat
 	lazy = false,
 	keys = {
 		{
-			"<leader>f",
+			"<leader>cf",
 			function()
 				require("conform").format({ async = true, lsp_fallback = true })
 			end,
 			mode = "",
-			desc = "[F]ormat buffer",
+			desc = "[C]ode [F]ormat buffer",
 		},
 	},
 	opts = {
 		notify_on_error = false,
-		format_on_save = function(bufnr)
-			-- Disable "format_on_save lsp_fallback" for languages that don't
-			-- have a well standardized coding style. You can add additional
-			-- languages here or re-enable it for the disabled ones.
-			local disable_filetypes = { c = true, cpp = true }
-			return {
-				timeout_ms = 500,
-				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-			}
-		end,
+		-- format_on_save = function(bufnr)
+		-- 	-- Disable "format_on_save lsp_fallback" for languages that don't
+		-- 	-- have a well standardized coding style. You can add additional
+		-- 	-- languages here or re-enable it for the disabled ones.
+		-- 	local disable_filetypes = { c = true, cpp = true }
+		-- 	return {
+		-- 		timeout_ms = 500,
+		-- 		lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+		-- 	}
+		-- end,
 		formatters_by_ft = {
 			lua = { "stylua" },
 			python = { "black" },
-			c = { "clang_format_new" },
 			-- sh = { "beautysh" },
 			-- zsh = { "beautysh" },
 			-- csh = { "beautysh" },
@@ -35,8 +34,8 @@ return { -- Autoformat
 			assembly = { "asmfmt" },
 			-- c = { "clang-format" },
 			-- cpp = { "clang-format" },
-			-- c = { "clang_format_new" },
-			-- cpp = { "clang_format_new" },
+			c = { "clang_format_new" },
+			cpp = { "clang_format_new" },
 			-- Conform can also run multiple formatters sequentially
 			--
 			-- You can use a sub-list to tell conform to run *until* a formatter
@@ -47,7 +46,7 @@ return { -- Autoformat
 			clang_format_new = {
 				command = "clang-format",
 				args = {
-					"-style={BasedOnStyle: Google, IndentWidth: 4, TabWidth: 4, UseTab: ForIndentation, ColumnLimit: 100}",
+					"-style={IncludeBlocks: Preserve, SortIncludes: Never, BasedOnStyle: Google, IndentWidth: 4, TabWidth: 4, UseTab: ForIndentation, ColumnLimit: 100}",
 				},
 				stdin = true,
 			},
