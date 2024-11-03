ed1c54f3b179e52ae7d8b2194007b0ab3db9cd95
Ostap4ello <okpeleh@gamil.com>
2024-11-03 22:59:32 +0100

    nvim: binds

--- .config/nvim/lua/plugins/keybindHinter.lua
+++ .config/nvim/lua/plugins/keybindHinter.lua
@@ -5,20 +5,16 @@ return { -- Useful plugin to show you pending keybinds.
 		require("which-key").setup()
 
 		-- DocumencopeFuzzyCommandSearch) existing key chains
-		require("which-key").register({
-			["<leader><leader>"] = { name = "View changed files", _ = "which_key_ignore" },
-			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-			["<leader>d"] = { name = "[D]ebug", _ = "which_key_ignore" },
-			--	["<leader>r"] = { name = "[R]ename", _ = "whichekey_ignore" },
-			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-			["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-			--	["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
-			-- ["<leader>t"] = { name = "Focus next [T]ab", _ = "which_key_ignore" },
+		require("which-key").add({
+			{ "<leader><leader>", group = "View changed files" },
+			{ "<leader>c", group = "[C]ode" },
+			{ "<leader>d", group = "[D]ebug" },
+			{ "<leader>s", group = "[S]earch" },
+			{ "<leader>w", group = "[W]orkspace" },
+			{ "<leader>t", group = "[T]oggle" },
+			-- { "<leader>r", group = "[R]ename" },
+			-- { "<leader>h", group = "Git [H]unk" },
+			-- { "<leader>t", group = "Focus next [T]ab" },
 		})
-		-- visual mode
-		-- require("which-key").register({
-		-- 	["<leader>h"] = { "Git [H]unk" },
-		-- }, { mode = "v" })
 	end,
 }
