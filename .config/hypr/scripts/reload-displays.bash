a4fa3d7228979c48a3823aad9c0e6202f915b80a
Ostap4ello <okpeleh@gamil.com>
2024-11-03 22:52:40 +0100

    hyprland - binds, script

---
+++ .config/hypr/scripts/reload-displays.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Reload displays
+# This script is used to reload the displays in the event that they are not
+# working properly. This script is for HYPRLAND.
+
+mv $HOME/.config/hypr/monitors.conf $HOME/.config/hypr/monitors.conf.bak
+touch $HOME/.config/hypr/monitors.conf # Touch the file to avoid errors
+hyprctl reload
+mv $HOME/.config/hypr/monitors.conf.bak $HOME/.config/hypr/monitors.conf
+hyprctl reload
