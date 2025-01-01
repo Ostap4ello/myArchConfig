f0054d844617e71aec71e64290659d48ec079f32
Ostap4ello <okpeleh@gamil.com>
2025-01-01 16:09:15 +0100

    fix: nvim: telescope: when opening a file, check if file is already opened. if y - open it's tab/focus

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -9,11 +9,11 @@ return {
 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 		vim.keymap.set("n", "<leader>sf", function()
-            builtin.find_files({no_ignore=true})
-        end, { desc = "[S]earch [F]iles" })
+			builtin.find_files({ no_ignore = true })
+		end, { desc = "[S]earch [F]iles" })
 		vim.keymap.set("n", "<leader>sg", function()
-            builtin.live_grep({no_ignore=true})
-        end, { desc = "[S]earch by [G]rep" })
+			builtin.live_grep({ no_ignore = true })
+		end, { desc = "[S]earch by [G]rep" })
 		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 		vim.keymap.set("n", "<leader>so", function()
@@ -41,16 +41,25 @@ return {
 
 		vim.keymap.set("n", "<leader>t", function()
 			vim.cmd("tab new")
-			builtin.find_files({no_ignore=true})
+			builtin.find_files({ no_ignore = true })
 		end, { desc = "Search files in New [T]ab" })
 
-        -- require("telescope").setup({
-        --     defaults = {
-                -- layout_config = {
-                --     prompt_position = "top",
-                -- },
-        --     },
-        -- })
+		require("telescope").setup({
+			defaults = {
+				mappings = {
+					i = {
+						["<CR>"] = function(bufnr)
+							require("telescope.actions.set").edit(bufnr, "tab drop")
+						end,
+					},
+					n = {
+						["<CR>"] = function(bufnr)
+							require("telescope.actions.set").edit(bufnr, "tab drop")
+						end,
+					},
+				},
+			},
+		})
 	end,
 	-- It's also possible to pass additional configuration options.
 	--  See `:help telescope.builtin.live_grep()` for information about particular keys
