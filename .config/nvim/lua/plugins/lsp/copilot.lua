61eecd7a232e6f1c510166f6fe3ccd181891745c
Ostap4ello <okpeleh@gamil.com>
2025-06-22 15:02:49 +0200

    fix: nvim: revert copilot changes

---
+++ .config/nvim/lua/plugins/lsp/copilot.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "zbirenbaum/copilot-cmp",
+        config = function ()
+        require("copilot_cmp").setup()
+        end
+    },
+    {
+        "github/copilot.vim",
+    }
+}
